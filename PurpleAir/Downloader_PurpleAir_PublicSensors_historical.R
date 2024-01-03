6d07c9db9fbb6805f64d10b82a7d6c36bc352d46
Zhiyong <61702831+Zhiyong319@users.noreply.github.com>
2024-01-02 19:59:20 -0500

    Update Downloader_PurpleAir_PublicSensors_historical.R

--- PurpleAir/Downloader_PurpleAir_PublicSensors_historical.R
+++ PurpleAir/Downloader_PurpleAir_PublicSensors_historical.R
@@ -1,17 +1,25 @@
-### set the environment
+# set the environment
 rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+# save screen outputs into a log file
+logfile <- paste0(format(Sys.time(), "%Y-%m-%d_%H_%M_%OS"), '.log')
+sink(logfile, append=TRUE, split=TRUE)
+
+# load libraries
 library(httr)
 library(jsonlite)
 library(openxlsx)
 # library(rjson)
 
-### sensor info list
-sensors <- read.xlsx('PurpleAirSensors20230713.xlsx', "SensorInfo")
+# sensor info list
+sensors <- read.csv('sensorInfo.csv') # columns: sensor_index, sensor_name
 # str(sensors)
 
-API_Key <- "xxxxx" 
+# set the path to save csv files
+savePath <- 'your_path/'
+
+API_Key <- "your_key"  
 
 # The map has history data period limits. They are as follows:
 #   
@@ -31,20 +39,20 @@ time_step <- 14*24*60*60 # in seconds
 
 for (id in 1:nrow(sensors)) {
   
-  # set the start and end time 
-  starttime <- as.POSIXct("2020-12-31 00:00:00 UTC", origin="1970-01-01", tz="UTC")
-  endtime   <- as.POSIXct("2023-07-01 23:00:00 UTC", origin="1970-01-01", tz="UTC")
+  # set the start and end time and create full time sequences 
+  starttime <- as.POSIXct("2023-07-02 00:00:00 UTC", origin="1970-01-01", tz="UTC")
+  endtime   <- as.POSIXct("2024-01-01 23:00:00 UTC", origin="1970-01-01", tz="UTC")
   
   rawdata <- data.frame(matrix(ncol = 0, nrow = 0)) # initiate the data.frame to store data
   while (starttime < endtime) {
    
-    pa_url <- paste("https://api.purpleair.com/v1/sensors/",sensors$sensor_index[id],"/history/csv?",sep="")
+    pa_url <- paste("https://api.purpleair.com/v1/sensors/",sensors$sensor.sensor_index[id],"/history/csv?",sep="")
     response <- GET(pa_url,
                 content_type_json(),
                 query = list(start_timestamp = unclass(starttime), 
                              end_timestamp = unclass(starttime)+time_step, 
                              average = time_interval,
-                             fields = "pm2.5_atm_a,pm2.5_atm_b,humidity,temperature,pressure"),
+                             fields = "pm2.5_atm_a,pm2.5_atm_b,humidity_a,temperature_a,pressure_a"),
                 add_headers('X-API-Key' = API_Key) )
     
     if (response$status_code==200) {
@@ -54,16 +62,16 @@ for (id in 1:nrow(sensors)) {
       # txt <- content(response, as="text", encoding="UTF-8")
       
       df <- read.table(text = txt, sep =",", header = TRUE, stringsAsFactors = FALSE)
-      rawdata <- rbind(rawdata, df)
       
-      if (nrow(df)>0) {
-        print(paste(sensors$sensor_index[id],starttime,'download GOOD, but no data avaiable!'))
+      if (nrow(df)==0) {
+        print(paste(sensors$sensor.sensor_index[id],starttime,'download GOOD, but no data avaiable!'))
       } else {
-        print(paste(sensors$sensor_index[id],starttime,'download GOOD!'))
+        rawdata <- rbind(rawdata, df)
+        print(paste(sensors$sensor.sensor_index[id],starttime,'download GOOD!'))
       }
       
     } else {
-      print(paste(sensors$sensor_index[id],starttime,'download FAILED!!!'))
+      print(paste(sensors$sensor.sensor_index[id],starttime,'download FAILED!!!'))
     }
 
     starttime <- starttime + time_step
@@ -73,15 +81,17 @@ for (id in 1:nrow(sensors)) {
   } # end of time loop
   
   # check if at least 1 row of data is available
-  if (nrow(rawdata)>1) {
+  if (nrow(rawdata)>=1) {
     rawdata$time <- as.POSIXct(rawdata$time_stamp, origin="1970-01-01", tz="UTC")
     
-    rawdata_out <- rawdata[,c('sensor_index','time','pm2.5_atm_a','pm2.5_atm_b','temperature','humidity','pressure')]
+    # re-order the columns
+    rawdata_out <- rawdata[,c('sensor_index','time','pm2.5_atm_a','pm2.5_atm_b','temperature_a','humidity_a','pressure_a')] 
     
-    filename <- paste(sensors$sensor_name[id],'_60min.csv',sep="")
+    filename <- paste(savePath,sensors$sensor.name[id],'_60min.csv',sep="")
     write.csv(rawdata_out,file=filename,row.names =FALSE)
+    
   } else {
-    print('!!!',paste(sensors$sensor_index[id],'no data avaiable during the defined period !!!'))
+    print(paste('!!!',sensors$sensor.sensor_index[id],'no data avaiable during the defined period !!!'))
   }
   
 } # end of sensor loop
